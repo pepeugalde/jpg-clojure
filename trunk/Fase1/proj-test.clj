c1eb13a8344a3759924fd0c68fbe10ebd797871d
pepe.ugalde <pepe.ugalde@30aee684-15a6-11df-9efb-f362119361b7>
2010-03-27 00:10:36 +0000

    Ya sirve update!

--- trunk/Fase1/proj-test.clj
+++ trunk/Fase1/proj-test.clj
@@ -3,41 +3,53 @@
  
 (def filenm       "db-1x2 - copia.db")
 (def testfilenm   "db-1x2 - copia.db")
-(def datab       (read-bin-file filenm))
+(def datab        (read-bin-file filenm))
 
  ;;Loading File
- (deftest loadfile
+(deftest loadfile
  (is (thrown? Exception (read-bin-file filenm)))) 
  
  ;;SimpleTest
- (deftest probando123
- 	 (println(is (= 3 (+ 1 1)))))	
+(deftest probando123
+ 	 (is (= 3 
+          (+ 1 
+             1))
+       "MATH FAIL"))
  
  ;;Loading Data from file into MAP 
- (deftest loaddata
+(deftest loaddata
  	 (testing "Testing - Loading Data from file into MAP" 
- 	 (is (instance? clojure.lang.PersistentArrayMap datab) "Loading Data into Map FAIL!"))
- 	)	
+ 	 (is (instance? clojure.lang.PersistentArrayMap 
+                  datab) 
+       "Loading Data into Map FAIL!")))	
  	
  ;;Loading Data from file 
- (deftest loaddata
- 	(is (instance? clojure.lang.PersistentArrayMap datab))) 
+(deftest load-data
+ 	(is (instance? clojure.lang.PersistentArrayMap 
+                 datab)
+      "Database should be a map!"))
 
  ;;Check Flipper
- (deftest inter-fliplr
- 	 (let [ tuple (to-array ["a" "b" "c" "d"])
- 	 	answer (to-array ["c" "b" "a" "d"])
-	 	left 0
-	 	right 2
-	 	exleftval (aget tuple left)
- 	 ]
- 	 (is (= (aget answer 1 ) (aget (fliplr tuple left right exleftval) 0)) "Testing Flipper")
- 	 
- 	 )
- )
+(deftest inter-fliplr
+ 	 (let [tuple  (to-array ["a" "b" "c" "d"])
+        answer  (to-array ["c" "b" "a" "d"])
+        left    0
+        right   2
+        exleftval (aget tuple left)
+       ]
+     (is (= (aget answer 1 ) 
+            (aget (fliplr tuple left right exleftval) 0))
+         "Flipper FAIL!")))
 
- 	
- 	
+ ;;Check Trims
+(deftest trim-val-test
+   (is (= (trim-value "roflolmao" 6)
+          "roflol")
+       "trim-value not trimming!"))
+(deftest get-trimmed-val-test
+   (is (= (get-trimmed-values ["abcde" "fghij" "klmno"][4 5 3]) 
+          ["abcd" "fghij" "klm"])
+       "FAILED to get trimmed values!"))
 
 (run-tests)
 
